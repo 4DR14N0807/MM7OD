d63f53589b9899793a1c4391da3a850fd9822c87
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2024-06-30 23:07:29 -0500

    Update RushWeapon.cs

--- srcnew/Weapons/RockWeapons/RushWeapon.cs
+++ srcnew/Weapons/RockWeapons/RushWeapon.cs
@@ -4,24 +4,22 @@ using System.Collections.Generic;
 namespace MMXOnline;
 
 public class RushWeapon : Weapon {
-    public RushWeapon() : base() {
-        maxAmmo = 7;
-        ammo = maxAmmo;
-    }
-
+	public RushWeapon() : base() {
+		maxAmmo = 7;
+		ammo = maxAmmo;
+	}
 
 	public override bool canShoot(int chargeLevel, Player player) {
 		Rock? rock = player.character as Rock;
-        if (rock.rush != null || rock == null) return false;
-        return true;
+		if (rock.rush != null || rock == null) return false;
+		return true;
 	}
-
 
 	public override void getProjectile(Point pos, int xDir, Player player, float chargeLevel, ushort netProjId) {
 		base.getProjectile(pos, xDir, player, chargeLevel, netProjId);
-        if (player.character is Rock rock) {
-            rock.rush = new Rush(pos, player, xDir, netProjId, true, true);
-            rock.rush.changeState(new RushJetState());
-        }
+		if (player.character is Rock rock) {
+			rock.rush = new Rush(pos, player, xDir, netProjId, true, true);
+			rock.rush.changeState(new RushJetState());
+		}
 	}
 }

9d8b18f416ab628b32e61314144ff3c19291988f
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2024-07-01 18:39:36 -0500

    Fixed bot spawn

--- src/Menu/AddBotMenu.cs
+++ src/Menu/AddBotMenu.cs
@@ -22,28 +22,35 @@ public class AddBotMenu : IMainMenu {
 	}
 
 	public void update() {
-		Helpers.menuUpDown(ref selectArrowPosY, 5, 5);
-
+		if (Global.level.gameMode.isTeamMode) {
+			Helpers.menuUpDown(ref selectArrowPosY, 0, 1);
+		}
 		if (selectArrowPosY == 0) {
 			if (Global.input.isPressedMenu(Control.MenuLeft)) {
 				botCharNum--;
-				if (botCharNum <= 5) {
-					botCharNum = 5;
+				if (botCharNum == 4) {
+					botCharNum = -1;
+				}
+				else if (botCharNum <= -1) {
+					botCharNum = 7;
 				}
 			} else if (Global.input.isPressedMenu(Control.MenuRight)) {
 				botCharNum++;
-				if (botCharNum >= 6) {
-					botCharNum = 6;
+				if (botCharNum == 0) {
+					botCharNum = 5;
+				}
+				else if (botCharNum > 7) {
+					botCharNum = -1;
 				}
 			}
 		}
 		if (selectArrowPosY == 1 && teamOptionEnabled()) {
 			if (Global.input.isPressedMenu(Control.MenuLeft)) {
 				botTeamNum--;
-				if (botTeamNum < -1) botTeamNum = -1;
+				if (botTeamNum < -1) { botTeamNum = -1; }
 			} else if (Global.input.isPressedMenu(Control.MenuRight)) {
 				botTeamNum++;
-				if (botTeamNum > 1) botTeamNum = 1;
+				if (botTeamNum >= Global.level.teamNum) { botTeamNum = Global.level.teamNum - 1; }
 			}
 		}
 
@@ -54,13 +61,14 @@ public class AddBotMenu : IMainMenu {
 				int id = 0;
 				int charNum = botCharNum;
 				int alliance = botTeamNum;
-				if (charNum == -1) charNum = 5;
+				if (charNum == -1) {
+					charNum = Helpers.randomRange(5, 7);
+				}
 				if (alliance == -1) {
 					if (Global.level.gameMode.isTeamMode) {
 						alliance = Server.getMatchInitAutobalanceTeam(Global.level.players, Global.level.teamNum);
 					}
 				}
-
 				for (int i = 0; i < Global.level.players.Count + 1; i++) {
 					if (!Global.level.players.Any(p => p.id == i)) {
 						id = i;
@@ -70,8 +78,9 @@ public class AddBotMenu : IMainMenu {
 						break;
 					}
 				}
-
-				var cpu = new Player("CPU" + id.ToString(), id, 6, null, true, true, alliance, new Input(true), null);
+				var cpu = new Player(
+					"CPU" + id.ToString(), id, charNum, null, true, true, alliance, new Input(true), null
+				);
 				Global.level.players.Add(cpu);
 				Global.level.gameMode.chatMenu.addChatEntry(new ChatEntry("Added bot " + cpu.name + ".", null, null, true));
 			}
@@ -87,30 +96,34 @@ public class AddBotMenu : IMainMenu {
 
 		Fonts.drawText(FontType.BlueMenu, "Add Bot", Global.halfScreenW, 15, Alignment.Center);
 
-
-		string botTeamStr = "(Auto)";
-		if (botTeamNum == GameMode.blueAlliance) botTeamStr = "Blue";
-		else if (botTeamNum == GameMode.redAlliance) botTeamStr = "Red";
-
-		string botCharStr = "(Random)";
-		if (botCharNum == 0) botCharStr = "Mega Man X";
-		else if (botCharNum == 1) botCharStr = "Zero";
-		else if (botCharNum == 2) botCharStr = "Vile";
-		else if (botCharNum == 3) botCharStr = "Axl";
-		else if (botCharNum == 4) botCharStr = "Sigma";
-		else if (botCharNum == 5) botCharStr = "Rockman";
-		else if (botCharNum == 6) botCharStr = "Protoman";
+		string botCharStr = botCharNum switch {
+			-1 => "(Random)",
+			0 => "Mega Man X",
+			1 => "Zero",
+			2 => "Vile",
+			3 => "Axl",
+			4 => "Sigma",
+			5 => "Mega Man",
+			6 => "Proto Man",
+			7 => "Bass",
+			_ => "Error"
+		};
 
 		Fonts.drawText(
 			FontType.Grey, "CHARACTER: " + botCharStr,
 			optionPoses[0].x, optionPoses[0].y, selected: selectArrowPosY == 0
 		);
-		Fonts.drawText(
-			teamOptionEnabled() ? FontType.Grey : FontType.Grey, "TEAM: " + botTeamStr,
-			optionPoses[1].x, optionPoses[1].y,
-			selected: selectArrowPosY == 1
-		);
-
+		if (Global.level.gameMode.isTeamMode) {
+			string botTeamStr = "(Auto)";
+			if (botTeamNum >= 0) {
+				botTeamStr = GameMode.getTeamName(botTeamNum);
+			}
+			Fonts.drawText(
+				teamOptionEnabled() ? FontType.Grey : FontType.Grey, "TEAM: " + botTeamStr,
+				optionPoses[1].x, optionPoses[1].y,
+				selected: selectArrowPosY == 1
+			);
+		}
 		Fonts.drawTextEX(
 			FontType.Grey, "Left/Right: Change, [OK]: Add, [BACK]: Back",
 			Global.screenW * 0.5f, 200, Alignment.Center

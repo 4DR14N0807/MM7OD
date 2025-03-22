411126f5f9cd7efd110521a752f13f796282dfde
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2025-03-22 12:33:21 -0500

    Fixed copy vision stack

--- srcnew/Bass/Weapons/CopyVision.cs
+++ srcnew/Bass/Weapons/CopyVision.cs
@@ -83,6 +83,7 @@ public class CopyVisionLemon : Projectile {
 
 		vel.x = 240 * xDir;
 		damager.damage = 1;
+		damager.hitCooldown = 9;
 
 		if (rpc) {
 			rpcCreate(pos, owner, ownerPlayer, netProjId, xDir);

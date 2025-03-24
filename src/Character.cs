17a95eb4666a99e0f8dda86a1cfff623045f7dee
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2025-03-24 13:55:01 -0500

    Fixed double jumo bug

--- src/Character.cs
+++ src/Character.cs
@@ -1463,22 +1463,13 @@ public partial class Character : Actor, IDamagable {
 				isDashing = (
 					isDashing || player.dashPressed(out string dashControl) && canDash()
 				);
+				changeState(new Jump());
 				if (isDashing) {
 					dashedInAir++;
 				}
-				changeState(new Jump());
 				return true;
 			} else if (player.dashPressed(out string dashControl) && canDash()) {
 				changeState(new Dash(dashControl), true);
-				return true;
-			} else if (
-				rideArmorPlatform != null &&
-				player.input.isPressed(Control.Jump, player) &&
-				player.input.isHeld(Control.Up, player) &&
-				canEjectFromRideArmor()
-			  ) {
-				getOffMK5Platform();
-				changeState(new Jump());
 				return true;
 			}
 			if (player.isCrouchHeld() && canCrouch() && charState is not Crouch) {

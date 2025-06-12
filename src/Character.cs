cc652e6dd9df6e5b728d473a2328ce2b8d83bef6
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2025-06-12 16:05:04 -0500

    Update Character.cs

--- src/Character.cs
+++ src/Character.cs
@@ -2255,10 +2255,15 @@ public partial class Character : Actor, IDamagable {
 			pos.x - 1, pos.y - 2, pos.x + 1, pos.y, true, new Color(0, 0, 0, 64), 1,
 			ZIndex.HUD, true, new Color(0, 0, 0, 125)
 		);
-		if (!ownedByLocalPlayer) {
-			Fonts.drawText(FontType.RedSmall,
-				$"{getActorTypeName()} Pos: {MathF.Round(pos.x)} {MathF.Round(pos.y)}",
-				10, 200
+		if (!ownedByLocalPlayer || true) {
+			Fonts.drawText(
+				FontType.RedSmall,
+				$"{getActorTypeName()} pos: {MathF.Round(pos.x)} {MathF.Round(pos.y)}" +
+				" v: " + (visible ? "0" : "1") + $" dir {xDir},{yDir}" +
+				$" anim: {sprite.name} {frameIndex}",
+				Global.level.camX + Global.screenW - 10, 2 + Global.level.camY,
+				Alignment.Right,
+				isWorldPos: true
 			);
 		}
 		if (!shouldRender(x, y)) {

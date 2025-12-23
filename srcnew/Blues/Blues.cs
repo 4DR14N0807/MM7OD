df2a945ceb8e1cfe5a40e076f3035f3abb0ed905
4DR14N0807 <4drian0807@gmail.com>
2025-12-23 14:14:22 -0400

    sound changes

--- srcnew/Blues/Blues.cs
+++ srcnew/Blues/Blues.cs
@@ -845,7 +845,7 @@ public class Blues : Character {
 				playSound("buster2", sendRpc: true);
 				addCoreAmmo(0.75f);
 			}
-			playSound("buster", sendRpc: true);
+			playSound("protoLemon", sendRpc: true);
 			lemonCooldown = 8;
 			unchargedLemonCooldown[lemonNum] = 40;
 			if (oldShootAnimTime <= 0.25f) {
